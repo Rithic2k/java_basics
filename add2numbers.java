5c60dbc6c2781e7c1268f5f1dc7b394373151c14
Rithic2k <118651756+Rithic2k@users.noreply.github.com>
2023-02-02 23:01:59 +0530

    Create add2numbers.java

---
+++ add2numbers.java
@@ -0,0 +1,14 @@
+package java_basic;
+
+public class Addtwonum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c;
+		System.out.println(c=a+b);
+
+	}
+
+}

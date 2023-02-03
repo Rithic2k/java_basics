943217c91ceee6cc54c7e8b1e953f02863397c25
Rithic2k <118651756+Rithic2k@users.noreply.github.com>
2023-02-03 22:01:24 +0530

    Add files via upload

---
+++ Data_types.java
@@ -0,0 +1,29 @@
+package java_basic;
+
+public class Data_types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		short s = 20;
+		long l = 11111111;
+		byte b = 1;
+		double d = 3.032343;
+		float f = 1.2f;
+		char c ='a';
+		boolean w = false;
+		System.out.println(s);
+		System.out.println(a);
+		System.out.println(l);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(w);
+		
+		
+		
+
+	}
+
+}

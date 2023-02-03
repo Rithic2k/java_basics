7a9b7c94ec71d13ed09e2cb82f05e8f407aaa758
Rithic2k <118651756+Rithic2k@users.noreply.github.com>
2023-02-03 22:31:36 +0530

    Add files via upload

---
+++ Arithmetic_and_precedence.java
@@ -0,0 +1,16 @@
+package java_basic;
+
+public class Arithmetic_and_precedence {
+
+	public static void main(String[] args) {
+		int a,b,c,d,e;
+		a = 10;
+		b = 3;
+		c =23;
+		d = 12;
+		e = a + (c*d/8)*b/a;
+		System.out.println(e);
+
+	}
+
+}
